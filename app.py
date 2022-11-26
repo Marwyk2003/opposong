ef324b2c120485b69fc887cef2a88f99d14d9f3e
Marcin Wykpis <marwyk2003@gmail.com>
2022-11-26 13:48:36 +0100

    Add ShazamWrapper Class and importing files

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import yaml
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from spotify import Client
+from shazam import ShazamWrapper
 
 app = Flask(__name__)
 
@@ -14,6 +15,13 @@ spotify_client = Client('user-read-currently-playing',
                         spotify_credentials['redirect_uri'])
 
 
-@app.route("/")
+@app.route("/", methods=['POST', 'GET'])
 def index():
-    return render_template('index.html', content={})
+    if request.method == 'POST':
+        f = request.files['file']
+        s = ShazamWrapper(f)
+        print(s.getData())
+    #   f.save(secure_filename(f.filename))
+        return 'file uploaded successfully'
+    else:
+        return render_template('index.html', content={})
--- shazam.py
+++ shazam.py
@@ -1,8 +1,9 @@
 from ShazamAPI import Shazam
 
-mp3_file_content_to_recognize = open('b.mp3', 'rb').read()
-
-shazam = Shazam(mp3_file_content_to_recognize)
-recognize_generator = shazam.recognizeSong()
-while True:
-    print(next(recognize_generator)) # current offset & shazam response to recognize requests
+class ShazamWrapper:
+    def __init__(self, file):
+        mp3_file_content_to_recognize = file.read()
+        shazam = Shazam(mp3_file_content_to_recognize)
+        self.recognize_generator = shazam.recognizeSong()
+    def getData(self):
+        return next(self.recognize_generator)

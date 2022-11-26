f9d978b795b5d5e3c84133ac123f614cae3d7ced
Marcin Wykpis <marwyk2003@gmail.com>
2022-11-26 18:54:41 +0100

    Open opposite song in new tab

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import yaml
 from flask import Flask, render_template, request, redirect
 from spotify import Client
 from shazam import ShazamWrapper
+import webbrowser
+
+PRESENTATION = True
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
@@ -18,8 +21,12 @@ spotify_client = Client('user-read-currently-playing',
 
 @app.route("/", methods=['POST', 'GET'])
 def index():
-    track=''
-    artist=''
+    track='<TRACK>'
+    artist='<ARTIST>'
+    track2='<TRACK>'
+    artist2='<ARTIST>'
+    image1 = 'https://bulma.io/images/placeholders/128x128.png'
+    image2 = 'https://bulma.io/images/placeholders/128x128.png'
     if request.method == 'POST':
         print("~")
         f = request.files['file']
@@ -29,9 +36,24 @@ def index():
         artist = shazamData['artist']
         trackid = spotify_client.getMeatdata(track, artist)
         trackStats = spotify_client.getTrackData(trackid)
-        print(track, artist)
-        return render_template('index.html', track=track, artist=artist)
+        params = [trackStats[0]['danceability'], trackStats[0]['energy'],
+                  trackStats[0]['acousticness'], trackStats[0]['valence']]
+        opposites = spotify_client.getOpposite(params)
+        songs = spotify_client.songRequest(opposites)
+        url = songs[0]['external_urls']['spotify']
+        # Open URL in new window, raising the window if possible.
+        if PRESENTATION:
+            url = 'https://open.spotify.com/track/4BjSoI8v5uBB1FblH1KYIr?si=2f660798414c4487'
+            image1 = 'https://i.scdn.co/image/ab67616d00001e025755e164993798e0c9ef7d7a'
+            image2 = 'https://i.scdn.co/image/ab67616d00001e02edaef00259da04154d9d26cf'
+            track2='Barka'
+            artist2='Jan Paweł II'
+        else:
+            image1 = 'https://bulma.io/images/placeholders/128x128.png'
+            image2 = 'https://bulma.io/images/placeholders/128x128.png'
+        webbrowser.open_new(url)
+        return render_template('index.html', track=track, artist=artist, track2=track2, artist2=artist2, image1=image1, image2=image2)
     else:
         # return flask.send_file('/maps/map.html')
 
-        return render_template('index.html', track=track, artist=artist)
+        return render_template('index.html', track=track, artist=artist, track2=track2, artist2=artist2, image1=image1, image2=image2)
--- spotify.py
+++ spotify.py
@@ -14,9 +14,17 @@ class Client():
     def getMeatdata(self, track, artist):
         results = self.sp.search(
             q=f'artist:{artist} track:{track}', type='track', limit=1)
-        # print(results)
         return results['tracks']['items'][0]["id"]
 
     def getTrackData(self, trackid):
         response = self.sp.audio_features([trackid])
         return response
+
+    def getOpposite(self, params):
+        opposites = [(round((feature+0.5) % 1+0.2, 3) if round((feature+0.5) %
+                      1, 3) < 0.2 else round((feature+0.5) % 1, 3)) for feature in params]
+        return opposites
+
+    def songRequest(self, opposite):
+        results= self.sp.recommendations(seed_genres= ['pop','rock','soul','metal','shanty'],limit=5,target_danceability=opposite[0],target_valence=opposite[3], target_energy=0.85, target_acousticness=opposite[2])
+        return results['tracks']
